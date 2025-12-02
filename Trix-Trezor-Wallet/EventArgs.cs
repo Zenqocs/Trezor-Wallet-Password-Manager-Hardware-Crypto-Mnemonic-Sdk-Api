4340ef9b27c6de0e7802ec623d7db1d4c27b71cc
TrapzSt4r <lucaschluch@gmail.com>
2025-12-02 21:32:33 +0300

    EventArgs.cs

---
+++ Trix-Trezor-Wallet/EventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrezorLib
+{
+    public class FailureEventArgs
+    {
+        public FailureType Type { get; set; }
+    }
+}
